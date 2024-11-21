4743122326d0c07fcc893200eaf684fe02c269ea
Sora-king <86035165+Sora-king@users.noreply.github.com>
2024-11-22 06:57:26 +0900

    音声と距離減衰を実装したつもり。同期でもうまくいくといいな。

--- Assets/AvatarSpawner.cs
+++ Assets/AvatarSpawner.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class AvatarSpawner : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private GameObject playerPrefab; // プレイヤーPrefab
+    [SerializeField] private Transform[] spawnPoints; // スポーン位置
 
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectUsingSettings(); // サーバーに接続
     }
 
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.JoinOrCreateRoom("RoomName", new RoomOptions(), TypedLobby.Default);
+        PhotonNetwork.JoinOrCreateRoom("RoomName", new RoomOptions(), TypedLobby.Default); // ルームに接続または作成
     }
 
     public override void OnJoinedRoom()
     {
+        // プレイヤーのスポーン位置を決定
         int playerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
         Vector3 spawnPosition = spawnPoints[playerIndex % spawnPoints.Length].position;
+
+        // アバターを生成
         PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
     }
 }
