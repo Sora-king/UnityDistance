730290ff6a7680062aa3e4e5d274dd4c58a248a3
Sora-king <86035165+Sora-king@users.noreply.github.com>
2024-11-21 14:27:02 +0900

    位置同期を再設定

--- Assets/AvatarSpawner.cs
+++ Assets/AvatarSpawner.cs
@@ -1,20 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class AvatarSpawner : MonoBehaviourPunCallbacks
 {
-    public GameObject avatarPrefab; // アバターPrefabを設定
+    [SerializeField]
+    GameObject networkPlayer;
+
+    [SerializeField]
+    Transform cameraRig;
+
+    [SerializeField]
+    Transform[] playerPositions; // 配列に全プレイヤーの位置情報を保持
+
+    GameObject player;
 
     void Start()
     {
-        // アバターを生成
-        if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom)
-        {
-            Vector3 spawnPosition = new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f));
-            PhotonNetwork.Instantiate(avatarPrefab.name, spawnPosition, Quaternion.identity);
-        }
+        // Photonサーバーに接続
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster() {
+        // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom() {
+        // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
+        var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate(networkPlayer.name, position, Quaternion.identity);
     }
 }
 
